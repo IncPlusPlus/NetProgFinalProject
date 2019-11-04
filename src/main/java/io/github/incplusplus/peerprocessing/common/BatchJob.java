f1be69ea44e1b620cbf6305cbec172527b82aa43
Ryan Cloherty <rcloherty@rocketsoftware.com>
2019-11-04 18:19:02 -0500

    Added query and batchjob class

---
+++ src/main/java/io/github/incplusplus/peerprocessing/common/BatchJob.java
@@ -0,0 +1,8 @@
+package io.github.incplusplus.peerprocessing.common;
+
+import java.util.List;
+
+public class BatchJob {
+	private List<Job> jobs;
+	
+}
---
+++ src/main/java/io/github/incplusplus/peerprocessing/common/Query.java
@@ -0,0 +1,5 @@
+package io.github.incplusplus.peerprocessing.common;
+
+public class Query<T> {
+
+}
