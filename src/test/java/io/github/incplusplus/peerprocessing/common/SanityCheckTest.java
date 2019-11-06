476cf6d9bbe148891dfc459c38a1ba1152d38e7b
Ryan Cloherty <rcloherty@rocketsoftware.com>
2019-11-06 12:13:25 -0500

    THE TESTS RUN!!!!!

---
+++ src/test/java/io/github/incplusplus/peerprocessing/common/SanityCheckTest.java
@@ -0,0 +1,12 @@
+package io.github.incplusplus.peerprocessing.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SanityCheckTest {
+	@Test
+	public void sanityCheck() {
+		assertEquals(1,1);
+	}
+}
