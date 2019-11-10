40e6a7496e6fba870debed601ed0f2d9f3b6b1b1
Ryan Cloherty <rcloherty@rocketsoftware.com>
2019-11-10 13:33:02 -0500

    Turn verbose output back off

--- src/test/java/io/github/incplusplus/peerprocessing/SingleSlaveIT.java
+++ src/test/java/io/github/incplusplus/peerprocessing/SingleSlaveIT.java
@@ -18,7 +18,7 @@ import java.util.concurrent.FutureTask;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SingleSlaveIT {
-	public static boolean VERBOSE_TEST_OUTPUT = true;
+	public static boolean VERBOSE_TEST_OUTPUT = false;
 	private static int serverPort;
 	
 	@BeforeAll
