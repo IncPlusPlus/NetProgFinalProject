dc707ae9792d2e8a2a57f48b49a089502630dd86
Ryan Cloherty <rcloherty@rocketsoftware.com>
2019-11-12 18:58:56 -0500

    Split Slave's main() into SlaveRunner

--- slave.sh
+++ slave.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./mvnw -q compile exec:java -Dexec.mainClass="io.github.incplusplus.peerprocessing.slave.Slave" -Dexec.cleanupDaemonThreads=false
+./mvnw -q compile exec:java -Dexec.mainClass="io.github.incplusplus.peerprocessing.slave.SlaveRunner" -Dexec.cleanupDaemonThreads=false
