a2fef31eac9817170fc0716327ab2456e5a593c0
Ryan Cloherty <rcloherty@rocketsoftware.com>
2019-11-12 21:42:46 -0500

    Optimize imports

--- src/main/java/io/github/incplusplus/peerprocessing/client/ClientRunner.java
+++ src/main/java/io/github/incplusplus/peerprocessing/client/ClientRunner.java
@@ -4,14 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.javatuples.Pair;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
 
 import static io.github.incplusplus.peerprocessing.common.MiscUtils.promptForHostPortTuple;
 import static io.github.incplusplus.peerprocessing.logger.StupidSimpleLogger.*;
