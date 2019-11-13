b59c27d776b976ec327483813cddba6658445d8d
Ryan Cloherty <rcloherty@rocketsoftware.com>
2019-11-12 19:38:35 -0500

    Add Client and Slave unit tests

---
+++ src/test/java/io/github/incplusplus/peerprocessing/slave/SlaveTest.java
@@ -0,0 +1,21 @@
+package io.github.incplusplus.peerprocessing.slave;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class SlaveTest {
+	
+	@Test
+	void failIfNoServer() {
+		Slave mySlave = new Slave("localhost", 1234);
+		boolean initSuccess = mySlave.init();
+		assert !initSuccess;
+		IllegalStateException thrown =
+				assertThrows(IllegalStateException.class,
+						mySlave::begin);
+		assertEquals(thrown.getMessage(), "Socket not initialized properly. " +
+				"Did you remember to check the boolean value of Client.begin()?!");
+	}
+}
