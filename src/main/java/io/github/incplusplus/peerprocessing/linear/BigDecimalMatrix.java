fdc0d284bb81cd3c3a630226a858b0e5db0a4c67
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-11-15 11:48:39 -0500

    Reformat with Google Java formatting

--- src/main/java/io/github/incplusplus/peerprocessing/linear/BigDecimalMatrix.java
+++ src/main/java/io/github/incplusplus/peerprocessing/linear/BigDecimalMatrix.java
@@ -7,36 +7,38 @@ import static io.github.incplusplus.peerprocessing.common.MiscUtils.randBigDec;
 import static java.math.BigDecimal.ZERO;
 
 public class BigDecimalMatrix extends RealMatrix<BigDecimal> {
-	private BigDecimal[][] matrix;
-	
-	BigDecimalMatrix(BigDecimal[][] matrix) {
-		this.matrix = matrix;
-	}
-	
-	BigDecimalMatrix() {
-		this.matrix = new BigDecimal[0][0];
-	}
-	
-	@Override
-	public RealMatrix<BigDecimal> random(int m, int n) {
-		BigDecimal[][] out = new BigDecimal[m][n];
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				out[i][j] = randBigDec(100000);
-			}
-		}
-		return new BigDecimalMatrix(out);
-	}
-	
-	@Override
-	public RealMatrix<BigDecimal> identity(int n) {
-		BigDecimal[][] out = initZero(new BigDecimal[n][n]);
-		for (int i = 0; i < n; i++) { out[i][i] = BigDecimal.ONE; }
-		return new BigDecimalMatrix(out);
-	}
-	
-	@Override
-	public RealMatrix<BigDecimal> multiply(RealMatrix<BigDecimal> other) {
+  private BigDecimal[][] matrix;
+
+  BigDecimalMatrix(BigDecimal[][] matrix) {
+    this.matrix = matrix;
+  }
+
+  BigDecimalMatrix() {
+    this.matrix = new BigDecimal[0][0];
+  }
+
+  @Override
+  public RealMatrix<BigDecimal> random(int m, int n) {
+    BigDecimal[][] out = new BigDecimal[m][n];
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        out[i][j] = randBigDec(100000);
+      }
+    }
+    return new BigDecimalMatrix(out);
+  }
+
+  @Override
+  public RealMatrix<BigDecimal> identity(int n) {
+    BigDecimal[][] out = initZero(new BigDecimal[n][n]);
+    for (int i = 0; i < n; i++) {
+      out[i][i] = BigDecimal.ONE;
+    }
+    return new BigDecimalMatrix(out);
+  }
+
+  @Override
+  public RealMatrix<BigDecimal> multiply(RealMatrix<BigDecimal> other) {
     if (this.getNumCols() != other.getNumRows())
       throw new IllegalArgumentException(
           "AB is not defined where A is "
@@ -47,94 +49,97 @@ public class BigDecimalMatrix extends RealMatrix<BigDecimal> {
               + other.getNumRows()
               + "x"
               + other.getNumCols());
-		BigDecimal[][] result = initZero(new BigDecimal[this.getNumRows()][other.getNumCols()]);
-		for (int i = 0; i < this.getNumRows(); i++) {
-			for (int j = 0; j < other.getNumCols(); j++) {
-				for (int k = 0; k < this.getNumCols(); k++) {
-					result[i][j] = result[i][j].add(matrix[i][k].multiply(other.getEntry(k, j)));
-				}
-			}
-		}
-		return new BigDecimalMatrix(result);
-	}
-	
-	@Override
-	public BigDecimal[] multiply(BigDecimal[] vector) {
-		int numRows = matrix.length;
-		int numCols = matrix[0].length;
-		if (vector.length != numCols) throw new RuntimeException("Illegal matrix dimensions.");
-		BigDecimal[] out = new BigDecimal[numRows];
-		for (int i = 0; i < numRows; i++) {
-			for (int j = 0; j < numCols; j++) { out[i] = out[i].add(matrix[i][j].multiply(vector[j])); }
-		}
-		return out;
-	}
-	
-	@Override
-	public BigDecimal[] getCol(int colIndex) {
-		//adapted from https://stackoverflow.com/a/30426991/1687436
-		BigDecimal[] column = new BigDecimal[matrix[0].length];
-		for (int i = 0; i < column.length; i++) {
-			column[i] = matrix[i][colIndex];
-		}
-		return column;
-	}
-	
-	@Override
-	public BigDecimal[] getRow(int rowIndex) {
-		return matrix[rowIndex];
-	}
-	
-	@Override
-	protected BigDecimal getEntry(int rowNum, int colNum) {
-		return matrix[rowNum][colNum];
-	}
-	
-	@Override
-	public BigDecimal dot(BigDecimal[] rowVector, BigDecimal[] columnVector) {
-		return null;
-	}
-	
-	@Override
-	public RealMatrix<BigDecimal> add(RealMatrix<BigDecimal> addend) {
-		if (!(getNumRows()!=addend.getNumRows()&&getNumCols()!=addend.getNumCols())) {
-			throw new IllegalArgumentException("A+B is not defined where A is "
-					+ getNumRows()
-					+ "x"
-					+ getNumCols()
-					+ " and B is "
-					+ addend.getNumRows()
-					+ "x"
-					+ addend.getNumCols());
-		}
-		BigDecimal[][] newMatrix = new BigDecimal[getNumRows()][getNumCols()];
-		for(int i = 0; i < getNumRows(); i++) {
-			for (int j = 0;j<getNumCols();j++){
-				newMatrix[i][j]=getEntry(i,j).add(addend.getEntry(i,j));
-			}
-		}
-		return new BigDecimalMatrix(newMatrix);
-	}
-	
-	@Override
-	public int getNumRows() {
-		return matrix.length;
-	}
-	
-	@Override
-	public int getNumCols() {
-		return matrix[0].length;
-	}
-	
-	@Override
-	public BigDecimal[][] getContents() {
-		return Arrays.copyOf(matrix, matrix.length);
-	}
-	
-	private static BigDecimal[][] initZero(BigDecimal[][] nullArray) {
-		for (BigDecimal[] bigDecimals : nullArray) {
-			Arrays.fill(bigDecimals, ZERO);
-		}
-		return nullArray;
-	}
+    BigDecimal[][] result = initZero(new BigDecimal[this.getNumRows()][other.getNumCols()]);
+    for (int i = 0; i < this.getNumRows(); i++) {
+      for (int j = 0; j < other.getNumCols(); j++) {
+        for (int k = 0; k < this.getNumCols(); k++) {
+          result[i][j] = result[i][j].add(matrix[i][k].multiply(other.getEntry(k, j)));
+        }
+      }
+    }
+    return new BigDecimalMatrix(result);
+  }
+
+  @Override
+  public BigDecimal[] multiply(BigDecimal[] vector) {
+    int numRows = matrix.length;
+    int numCols = matrix[0].length;
+    if (vector.length != numCols) throw new RuntimeException("Illegal matrix dimensions.");
+    BigDecimal[] out = new BigDecimal[numRows];
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+        out[i] = out[i].add(matrix[i][j].multiply(vector[j]));
+      }
+    }
+    return out;
+  }
+
+  @Override
+  public BigDecimal[] getCol(int colIndex) {
+    // adapted from https://stackoverflow.com/a/30426991/1687436
+    BigDecimal[] column = new BigDecimal[matrix[0].length];
+    for (int i = 0; i < column.length; i++) {
+      column[i] = matrix[i][colIndex];
+    }
+    return column;
+  }
+
+  @Override
+  public BigDecimal[] getRow(int rowIndex) {
+    return matrix[rowIndex];
+  }
+
+  @Override
+  protected BigDecimal getEntry(int rowNum, int colNum) {
+    return matrix[rowNum][colNum];
+  }
+
+  @Override
+  public BigDecimal dot(BigDecimal[] rowVector, BigDecimal[] columnVector) {
+    return null;
+  }
+
+  @Override
+  public RealMatrix<BigDecimal> add(RealMatrix<BigDecimal> addend) {
+    if (!(getNumRows() != addend.getNumRows() && getNumCols() != addend.getNumCols())) {
+      throw new IllegalArgumentException(
+          "A+B is not defined where A is "
+              + getNumRows()
+              + "x"
+              + getNumCols()
+              + " and B is "
+              + addend.getNumRows()
+              + "x"
+              + addend.getNumCols());
+    }
+    BigDecimal[][] newMatrix = new BigDecimal[getNumRows()][getNumCols()];
+    for (int i = 0; i < getNumRows(); i++) {
+      for (int j = 0; j < getNumCols(); j++) {
+        newMatrix[i][j] = getEntry(i, j).add(addend.getEntry(i, j));
+      }
+    }
+    return new BigDecimalMatrix(newMatrix);
+  }
+
+  @Override
+  public int getNumRows() {
+    return matrix.length;
+  }
+
+  @Override
+  public int getNumCols() {
+    return matrix[0].length;
+  }
+
+  @Override
+  public BigDecimal[][] getContents() {
+    return Arrays.copyOf(matrix, matrix.length);
+  }
+
+  private static BigDecimal[][] initZero(BigDecimal[][] nullArray) {
+    for (BigDecimal[] bigDecimals : nullArray) {
+      Arrays.fill(bigDecimals, ZERO);
+    }
+    return nullArray;
+  }
 }
