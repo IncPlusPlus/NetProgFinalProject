02474ea684835e880befda530ad54762b78bea8b
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-11-22 20:33:24 -0500

    Added some tests for the ProperClient interface

--- src/test/java/io/github/incplusplus/peerprocessing/slave/SlaveTest.java
+++ src/test/java/io/github/incplusplus/peerprocessing/slave/SlaveTest.java
@@ -2,15 +2,13 @@ package io.github.incplusplus.peerprocessing.slave;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SlaveTest {
-	
-	@Test
-	void failIfNoServer() {
-		Slave mySlave = new Slave("localhost", 9999);
-		assertThrows(java.net.ConnectException.class,
-				mySlave::begin);
-	}
+
+  @Test
+  void failIfNoServer() {
+    Slave mySlave = new Slave("localhost", 9999);
+    assertThrows(java.net.ConnectException.class, mySlave::begin);
+  }
 }
