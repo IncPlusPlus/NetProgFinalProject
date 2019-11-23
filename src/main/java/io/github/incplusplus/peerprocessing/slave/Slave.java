d1ac1238b0346025e066525a57afa9dd8578f9e8
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-11-23 16:28:20 -0500

    Revert "Try manual flush"
    This reverts commit eb325aee

--- src/main/java/io/github/incplusplus/peerprocessing/slave/Slave.java
+++ src/main/java/io/github/incplusplus/peerprocessing/slave/Slave.java
@@ -126,7 +126,6 @@ public class Slave implements ProperClient, Personable {
 		boolean notAlreadyClosed = running.compareAndSet(true, false);
 		assert notAlreadyClosed;
 		outToServer.println(DISCONNECT);
-		outToServer.flush();
 		kill();
 	}
 	
