fa81309e4c1eba09c1402a64bd54659755669772
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-12-05 00:11:08 -0500

    Try using sh instead of bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export ZEROTIER_NETWORK_ID=9f77fc393e90f6ef
 
-curl -s https://install.zerotier.com | sudo bash
+curl -s https://install.zerotier.com | sudo sh
 sudo zerotier-cli join $ZEROTIER_NETWORK_ID
 echo running "$@"
-bash "$@"
+sh "$@"
 sudo zerotier-cli leave $ZEROTIER_NETWORK_ID
