f57f4f1d7bb8fa5706b5fbdc2a61e8079d752197
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-12-05 00:03:05 -0500

    I'm running out of ideas

--- persistentSlaveOneLiner.sh
+++ persistentSlaveOneLiner.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t persistentslave:tag -f PersistentSlave.Dockerfile . && docker run --rm -it persistentslave:tag
+docker build -t persistentslave:tag -f PersistentSlave.Dockerfile .; docker run --rm -it persistentslave:tag
