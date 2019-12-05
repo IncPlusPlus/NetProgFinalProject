2fe94f9ece5e30ef60d9f5c9414c266b7d01a4b7
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-12-04 23:28:44 -0500

    Let persistentSlave.sh accept host and port arguments

--- persistentSlave.sh
+++ persistentSlave.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./mvnw -q compile exec:java -Dexec.mainClass="io.github.incplusplus.peerprocessing.slave.PersistentSlaveRunner" -Dexec.cleanupDaemonThreads=false
+./mvnw -q compile exec:java -Dexec.mainClass="io.github.incplusplus.peerprocessing.slave.PersistentSlaveRunner" -Dexec.args="$*" -Dexec.cleanupDaemonThreads=false
