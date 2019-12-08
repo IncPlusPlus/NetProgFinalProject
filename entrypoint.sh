aa174e2d33f884bf64ecfefbcec7c408ff54e077
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-12-08 18:07:56 -0500

    Revert "Try some stupid stuff"
    This reverts commit 70b2c6ed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,6 @@ export ZEROTIER_NETWORK_ID=9f77fc393e90f6ef
 curl -s https://install.zerotier.com | sudo sh
 sudo zerotier-cli join $ZEROTIER_NETWORK_ID
 sudo zerotier-cli listnetworks
-sudo apt install zerotier-one -y --allow-unauthenticated
-sudo service zerotier-one start
-sudo zerotier-cli join $ZEROTIER_NETWORK_ID
-sudo zerotier-cli listnetworks
 echo running "$@"
 sh "$@"
 sudo zerotier-cli leave $ZEROTIER_NETWORK_ID
