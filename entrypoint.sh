0c6f64478f0a572097421afd1d24b729962259f0
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-12-08 16:45:33 -0500

    Try troubleshooting failed join

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ export ZEROTIER_NETWORK_ID=9f77fc393e90f6ef
 
 curl -s https://install.zerotier.com | sudo sh
 sudo zerotier-cli join $ZEROTIER_NETWORK_ID
+sudo zerotier-cli listnetworks
 echo running "$@"
 sh "$@"
 sudo zerotier-cli leave $ZEROTIER_NETWORK_ID
