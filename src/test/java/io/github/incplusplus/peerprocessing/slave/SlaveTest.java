b715fa4ce71273d4227b51ccca7c23e8395fc295
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-12-11 17:01:21 -0500

    Added new test case to ensure a Slave dies if it can't send an answer

--- src/test/java/io/github/incplusplus/peerprocessing/slave/SlaveTest.java
+++ src/test/java/io/github/incplusplus/peerprocessing/slave/SlaveTest.java
@@ -6,7 +6,14 @@ import static io.github.incplusplus.peerprocessing.slave.PersistentSlaveRunner.r
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import io.github.incplusplus.peerprocessing.query.Query;
 import io.github.incplusplus.peerprocessing.server.Server;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -21,6 +28,44 @@ class SlaveTest {
   void failIfNoServer() {
     Slave mySlave = new Slave("localhost", 9999);
     assertThrows(java.net.ConnectException.class, mySlave::begin);
+  }
+
+  /**
+   * If the unthinkable happens and a {@link JsonProcessingException} gets thrown when the Slave
+   * sends a {@link Query} back to the server, ensure the slave slays itself.
+   *
+   * @throws IOException if something even dumber happens
+   */
+  @Test
+  void dieIfCantSendAnswer() throws IOException {
+    // I would use a mock() here but we need to configure the ObjectMapper
+    // instance. Also, it's not just used in this method so it needs to work
+    // without interfering with its other uses.
+    ObjectMapper mapper =
+        spy(
+            new ObjectMapper()
+                .activateDefaultTyping(BasicPolymorphicTypeValidator.builder().build()));
+    Server server = new Server();
+    int port = server.start(0, VERBOSE_TEST_OUTPUT);
+    Slave mySlave = new Slave("localhost", port, mapper);
+    mySlave.setVerbose(VERBOSE_TEST_OUTPUT);
+    mySlave.begin();
+    //noinspection StatementWithEmptyBody
+    while (!mySlave.isPolite()) {}
+    when(mapper.writeValueAsString(any()))
+        .thenThrow(
+            new JsonProcessingException("This exception is expected as a test. Leave it be.") {});
+    mySlave.sendEvaluatedQuery(new DisallowedQueryExtension());
+    assertTrue(mySlave.isClosed());
+    server.stop();
+  }
+
+  static final class DisallowedQueryExtension extends Query {
+    private DisallowedQueryExtension() {}
+
+    public void complete() {
+      throw new RuntimeException();
+    }
   }
 
   @Test
